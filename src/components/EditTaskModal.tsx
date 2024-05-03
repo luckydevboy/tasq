dba7c3f58975c0676b7f4d687af7a09a03417a3c
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-05-03 19:37:35 +0330

    feat: implement drawer for edit task modal in mobile view

--- src/components/EditTaskModal.tsx
+++ src/components/EditTaskModal.tsx
@@ -4,6 +4,8 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import Drawer from "react-modern-drawer";
+import { useMediaQuery } from "react-responsive";
 
 import { Task } from "@/interfaces";
 import { Button, Modal } from "@/components";
@@ -33,11 +35,15 @@ const EditTaskModal = ({ isOpen, handleClose, task }: Props) => {
   const unCompleteTask = useUnCompleteTask();
   const queryClient = useQueryClient();
 
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1025px)",
+  });
+
   const onSubmit: SubmitHandler<TaskModify> = async (data) => {
     try {
       await updateTask.mutateAsync({ taskId: task._id, task: data });
       toast.success("تسک با موفقیت ویرایش شد.");
-      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      await queryClient.invalidateQueries({ queryKey: ["tasks"] });
       handleClose();
     } catch (error) {
       toast.error("مشکلی پیش آمده. مجددا تلاش کنید.");
@@ -53,7 +59,7 @@ const EditTaskModal = ({ isOpen, handleClose, task }: Props) => {
         await completeTask.mutateAsync(task._id);
         toast.success("تسک با موفقیت تکمیل شد.");
       }
-      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      await queryClient.invalidateQueries({ queryKey: ["tasks"] });
       handleClose();
     } catch (error) {
       toast.error("مشکلی پیش آمده. مجددا تلاش کنید.");
@@ -64,81 +70,99 @@ const EditTaskModal = ({ isOpen, handleClose, task }: Props) => {
     try {
       await deleteTask.mutateAsync(task._id);
       toast.success("تسک به سطل آشغال اضافه شد.");
-      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      await queryClient.invalidateQueries({ queryKey: ["tasks"] });
       handleClose();
     } catch (error) {
       toast.error("مشکلی پیش آمده. مجددا تلاش کنید.");
     }
   };
 
-  return (
-    <Modal isOpen={isOpen} handleClose={handleClose} title="ویرایش تسک">
-      <form
-        className="grid grid-cols-1 gap-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div>
-          <input
-            type="text"
-            placeholder="عنوان"
-            {...register("title", { required: "عنوان اجباری است!" })}
-            className="input"
-          />
-          {errors.title && (
-            <small className="text-red-500">{errors.title.message}</small>
-          )}
-        </div>
-        <div>
-          <input
-            className="input"
-            type="text"
-            placeholder="سررسید"
-            {...register("dueDate")}
-          />
-        </div>
-        <textarea
-          className="input self-stretch resize-none"
-          placeholder="توضیحات"
-          {...register("description")}
+  const renderForm = () => (
+    <form
+      className="grid grid-cols-1 gap-4 p-4 lg:p-0"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div>
+        <input
+          type="text"
+          placeholder="عنوان"
+          {...register("title", { required: "عنوان اجباری است!" })}
+          className="input"
         />
-        <div className="flex justify-between">
-          <div className="flex gap-x-2 items-center">
-            <Button
-              variant="outlined"
-              color="danger"
-              onClick={handleDelete}
-              isLoading={deleteTask.isPending}
-              type="button"
-            >
-              <span className="flex items-center gap-x-1">
-                <TrashIcon className="text-red-700 w-4 h-4" />
-                حذف
-              </span>
-            </Button>
-            <Button
-              variant="outlined"
-              color="success"
-              onClick={handleComplete}
-              isLoading={completeTask.isPending}
-              type="button"
-            >
-              <span className="flex items-center gap-x-1">
-                <CheckIcon className="text-green-700 w-4 h-4" />
-                {task.completed ? "عدم تکمیل" : "تکمیل"}
-              </span>
-            </Button>
-          </div>
+        {errors.title && (
+          <small className="text-red-500">{errors.title.message}</small>
+        )}
+      </div>
+      <div>
+        <input
+          className="input"
+          type="text"
+          placeholder="سررسید"
+          {...register("dueDate")}
+        />
+      </div>
+      <textarea
+        className="input self-stretch resize-none"
+        placeholder="توضیحات"
+        {...register("description")}
+      />
+      <div className="flex justify-between mt-auto">
+        <div className="flex gap-x-2 items-center">
           <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            isLoading={updateTask.isPending}
+            variant="outlined"
+            color="danger"
+            onClick={handleDelete}
+            isLoading={deleteTask.isPending}
+            type="button"
           >
-            ویرایش
+            <span className="flex items-center gap-x-1">
+              <TrashIcon className="text-red-700 w-4 h-4" />
+              حذف
+            </span>
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={handleComplete}
+            isLoading={completeTask.isPending}
+            type="button"
+          >
+            <span className="flex items-center gap-x-1">
+              <CheckIcon className="text-green-700 w-4 h-4" />
+              {task.completed ? "عدم تکمیل" : "تکمیل"}
+            </span>
           </Button>
         </div>
-      </form>
-    </Modal>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          isLoading={updateTask.isPending}
+        >
+          ویرایش
+        </Button>
+      </div>
+    </form>
+  );
+
+  return (
+    <>
+      {isDesktop ? (
+        <Modal isOpen={isOpen} handleClose={handleClose} title="ویرایش تسک">
+          {renderForm()}
+        </Modal>
+      ) : (
+        <Drawer
+          open={isOpen}
+          onClose={handleClose}
+          direction="bottom"
+          size={290}
+          className="rounded-t-lg"
+        >
+          {renderForm()}
+        </Drawer>
+      )}
+    </>
   );
 };
 
