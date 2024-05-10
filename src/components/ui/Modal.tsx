cb9a828fdd6a73f02248b0b5d3f6b34907098a54
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-05-10 23:54:45 +0330

    fix: click on the date picker will lead to closing the model

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { ReactNode, useRef } from "react";
-import { useClickAway } from "react-use";
+import { ReactNode } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {
@@ -14,12 +13,6 @@ type Props = {
 };
 
 const Modal = ({ children, title, isOpen, handleClose }: Props) => {
-  const ref = useRef(null);
-
-  useClickAway(ref, () => {
-    handleClose();
-  });
-
   return (
     <AnimatePresence>
       {isOpen && (
@@ -29,13 +22,13 @@ const Modal = ({ children, title, isOpen, handleClose }: Props) => {
             animate={{ opacity: "25%" }}
             exit={{ opacity: 0 }}
             className="fixed inset-0 bg-black"
+            onClick={handleClose}
           ></motion.div>
           <motion.div
             className="bg-white fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 rounded-lg w-full max-w-[450px]"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            ref={ref}
           >
             {title && <h1 className="text-lg font-bold mb-4">{title}</h1>}
             {children}
